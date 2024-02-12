993fb05ef5f96aa0f4dcc5daa69c8205b5bf390c
Chimaux <ugoppromise@gmail.com>
2024-02-12 12:20:21 +0100

    second teacher commit

---
+++ teacher/datatype.js
@@ -0,0 +1,26 @@
+let a = true
+let b = 3
+let c = 'go'
+let d = 5 > 6
+let e = 66.8
+let f
+let h = null
+
+
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
+console.log(typeof(h))
+
+
+let array1= [1,2,3,4,5]
+let object_new={a:1,b:2,c:4,f:10}
+let newone = e.toString()
+// console.log(array1.length)
+// console.log(typeof newone)
+console.log(array1[4])
+console.log(object_new.f)
