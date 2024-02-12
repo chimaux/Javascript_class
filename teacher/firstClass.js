015be1c5675aca5ddede2f41f4f8ba61be1ba0c7
Chimaux <ugoppromise@gmail.com>
2024-02-12 09:42:03 +0100

    initial commit

---
+++ teacher/firstClass.js
@@ -0,0 +1,28 @@
+
+console.log("FIND THE AREA OF A TRIANGLE")
+
+// let base = prompt("Enter the base of the triangle")
+// let height = prompt("enter the height of the triangle")
+// const halve = 1/2
+// let answer = halve*base*height
+// // console.log(answer)
+// alert(1/2*(base*height))
+// // alert(answer)
+
+// let a=prompt("enter the value of \"a\"")
+// let b=prompt("enter the value of \"b\"")
+// let c=prompt("enter the value of \"c\"")
+// let b24ac = (Math.pow(b,2)) -(4*a*c)
+
+// let square_it = Math.sqrt(b24ac)
+
+// let positive_numerator = -b+square_it
+// let nagative_numerator = -b-square_it
+// console.log(positive_numerator)
+// let positive_answer = positive_numerator/(2*a)
+// let negative_answer = nagative_numerator/(2*a)
+
+
+// console.log("x1= "+positive_answer+", x2= "+negative_answer)
+
+let  text = "203, wait, 30, do"
