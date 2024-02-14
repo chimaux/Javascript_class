5bfa33f91dddab6346a0e9aca83bac7aedd72869
Chimaux <ugoppromise@gmail.com>
2024-02-14 22:48:58 +0100

    Revert "Delete senior_dev directory"
    This reverts commit 6b9cde7b86cedc6f4154428f6efc9b7f4f61a847.

---
+++ senior_dev/datatype.js
@@ -0,0 +1,18 @@
+let a = true
+let b = 3
+let c = 'go'
+let d = 5 > 6
+let e = 66.8
+let f
+let h = null
+ 
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
+console.log(typeof(h))
+
+
